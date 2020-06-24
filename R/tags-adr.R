1d2be21dbea9722fb33a342cb5c6e522b5fe05c2
herreio <git@herre.io>
2020-06-24 17:21:44 +0200

    adr printer gnd

--- R/tags-adr.R
+++ R/tags-adr.R
@@ -3,16 +3,14 @@
 #' @param item Item data parsed from picaxml
 #' @export
 adr_printer <- function(item) {
-  # 
   paste(tag_subf(item, "033J", "d"),
     tag_subf(item, "033J", "a"))
 }
 
-#' Normierter Verleger / Drucker (Alte Drucke)
+#' Normierter Verleger / Drucker GND (Alte Drucke)
 #'
 #' @param item Item data parsed from picaxml
 #' @export
 adr_printer_gnd <- function(item) {
-  # Normierter Verleger / Drucker (Alte Drucke)
   tag_subf(item, "033J", "0")
 }
