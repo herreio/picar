d857cc9002e12401e4e030a5947178ca23e17a71
herreio <git@herre.io>
2020-06-24 17:29:36 +0200

    vd18_number

--- R/tags-vd1X.R
+++ R/tags-vd1X.R
@@ -1,8 +1,7 @@
-#' ...
+#' VD-18 Nummer
 #'
 #' @param item Item data parsed from picaxml
 #' @export
-vd18_nr <- function(item) {
-  # VD-18 Nummer
+vd18_number <- function(item) {
   gsub("VD18 ", "", tag_subf(item, "006M", "0"), fixed=TRUE)
 }
