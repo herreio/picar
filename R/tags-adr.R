8d4217346de754ad29d1e76e6cb1fde743aabf3b
herreio <git@herre.io>
2020-06-27 01:38:19 +0200

    tags adr updated

--- R/tags-adr.R
+++ R/tags-adr.R
@@ -1,16 +1,24 @@
 #' Normierter Verleger / Drucker (Alte Drucke)
 #'
 #' @param item Item data parsed from picaxml
+#' @param collapse Whether to collapse multiple values
 #' @export
-adr_printer <- function(item) {
-  paste(tag_subf(item, "033J", "d"),
-    tag_subf(item, "033J", "a"))
+adr_printer <- function(item, collapse=FALSE) {
+    if (!is.null(tag_subf(item, "033J", "d")) & !is.null(tag_subf(item, "033J", "a"))) {
+        printer_name <- paste(tag_subf(item, "033J", "d", preserve=TRUE), tag_subf(item, "033J", "a", preserve=TRUE))
+        if(collapse) {
+            return(collapse_values(printer_name))
+        }
+        return(printer_name)
+    }
 }
 
 #' Normierter Verleger / Drucker GND (Alte Drucke)
 #'
 #' @param item Item data parsed from picaxml
+#' @param preserve Whether to preserve empty values
+#' @param collapse Whether to collapse multiple values
 #' @export
-adr_printer_gnd <- function(item) {
-  tag_subf(item, "033J", "0")
+adr_printer_gnd <- function(item, preserve=FALSE, collapse=FALSE) {
+  tag_subf(item, "033J", "0", preserve=preserve, collapse=collapse)
 }
