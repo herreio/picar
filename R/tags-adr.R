ec074141f1cce668b98e108e7f9011481171bf1c
herreio <git@herre.io>
2020-07-07 14:29:31 +0200

    adr updated

--- R/tags-adr.R
+++ R/tags-adr.R
@@ -11,8 +11,18 @@ adr_printer <- function(item, collapse=FALSE) {
         }
         return(printer_name)
     }
+    if (!is.null(tag_subf(item, "033J", "D")) & !is.null(tag_subf(item, "033J", "A"))) {
+        printer_name <- paste(tag_subf(item, "033J", "D", preserve=TRUE), tag_subf(item, "033J", "A", preserve=TRUE))
+        if(collapse) {
+            return(collapse_values(printer_name))
+        }
+        return(printer_name)
+    }
     if (is.null(tag_subf(item, "033J", "d")) & !is.null(tag_subf(item, "033J", "a"))) {
         return(tag_subf(item, "033J", "a", collapse=collapse))
+    }
+    if (is.null(tag_subf(item, "033J", "D")) & !is.null(tag_subf(item, "033J", "A"))) {
+        return(tag_subf(item, "033J", "A", collapse=collapse))
     }
 }
 
@@ -23,5 +33,10 @@ adr_printer <- function(item, collapse=FALSE) {
 #' @param collapse Whether to collapse multiple values
 #' @export
 adr_printer_gnd <- function(item, preserve=FALSE, collapse=FALSE) {
-  tag_subf(item, "033J", "0", preserve=preserve, collapse=collapse)
+  if (!is.null(tag_subf(item, "033J", "0"))) {
+      return(tag_subf(item, "033J", "0", preserve=preserve, collapse=collapse))
+  }
+  if (!is.null(tag_subf(item, "033J", "7"))) {
+      return(tag_subf(item, "033J", "7", preserve=preserve, collapse=collapse))
+  }
 }
