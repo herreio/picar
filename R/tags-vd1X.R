6065e41d43387f8638e23fc699c2747366b1bcde
herreio <git@herre.io>
2020-07-21 15:46:17 +0200

    tags-vd1X updated

--- R/tags-vd1X.R
+++ R/tags-vd1X.R
@@ -3,12 +3,12 @@
 #' @param item Item data parsed from picaxml
 #' @export
 vd17_number <- function(item) {
-  tag_subf(item, "006W", "0")
+  gsub("VD17 ","", tag_subf(item, "006W", "0", collapse=TRUE), fixed=TRUE)
 }
 #' VD-18 Nummer
 #'
 #' @param item Item data parsed from picaxml
 #' @export
 vd18_number <- function(item) {
-  gsub("VD18 ", "", tag_subf(item, "006M", "0"), fixed=TRUE)
+  gsub("VD18 ", "", tag_subf(item, "006M", "0", collapse=TRUE), fixed=TRUE)
 }
